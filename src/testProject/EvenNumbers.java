b067a55eebb3596cb0ef1becaa608b568215fb2f
funshonami <54889826+funshonami@users.noreply.github.com>
2020-02-11 17:58:51 +0100

    First commit

---
+++ src/testProject/EvenNumbers.java
@@ -0,0 +1,16 @@
+package testProject;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		int count = 0;
+		
+		for(int i = 1; i < 10;  i++) {
+			if( i % 2 == 0){
+                System.out.print(i + " ");
+			
+		}
+
+	}
+	}
+}
